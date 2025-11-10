947ab8aa7001dedfbf19ed8f1605d58c4ac49ddb
prasad <prasad.prasad@y>
2025-11-10 15:46:41 +0530

    sql version_updated

--- data_tools/data_tools/page/partial_backup/partial_backup.js
+++ data_tools/data_tools/page/partial_backup/partial_backup.js
@@ -13,6 +13,7 @@ class PartialBackupPage {
 		this.page = page;
 		this.doctypes = [];
 		this.selected_doctypes = [];
+		this.export_format = 'json'; // Default format
 		this.make_page();
 	}
 
@@ -24,9 +25,19 @@ class PartialBackupPage {
 						<h4>Select DocTypes for Backup</h4>
 					</div>
 					<div class="frappe-card-body">
-						<div class="form-group">
-							<label>Filter by Module</label>
-							<div id="module-filter"></div>
+						<div class="row">
+							<div class="col-sm-6">
+								<div class="form-group">
+									<label>Filter by App</label>
+									<div id="app-filter"></div>
+								</div>
+							</div>
+							<div class="col-sm-6">
+								<div class="form-group">
+									<label>Filter by Module</label>
+									<div id="module-filter"></div>
+								</div>
+							</div>
 						</div>
 						<div class="form-group">
 							<label>Search DocTypes</label>
@@ -42,6 +53,10 @@ class PartialBackupPage {
 							<div id="doctype-list" style="max-height: 400px; overflow-y: auto; border: 1px solid #d1d8dd; padding: 10px; border-radius: 4px; background: white;"></div>
 						</div>
 						<div class="form-group">
+							<label>Export Format</label>
+							<div id="export-format"></div>
+						</div>
+						<div class="form-group">
 							<button class="btn btn-primary btn-sm" id="create-backup-btn">
 								<span class="fa fa-download"></span> Create Backup
 							</button>
@@ -52,9 +67,22 @@ class PartialBackupPage {
 			</div>
 		`);
 
+		this.load_apps();
 		this.load_modules();
 		this.load_doctypes();
+		this.setup_export_format();
 		this.setup_handlers();
+	}
+
+	load_apps() {
+		frappe.call({
+			method: 'data_tools.data_tools.page.partial_backup.partial_backup.get_apps',
+			callback: (r) => {
+				if (r.message) {
+					this.setup_app_filter(r.message);
+				}
+			}
+		});
 	}
 
 	load_modules() {
@@ -68,18 +96,60 @@ class PartialBackupPage {
 		});
 	}
 
-	load_doctypes(module_filter = null) {
-		frappe.call({
-			method: 'data_tools.data_tools.page.partial_backup.partial_backup.get_all_doctypes',
-			callback: (r) => {
-				if (r.message) {
-					this.doctypes = r.message;
-					this.filtered_doctypes = module_filter
-						? this.doctypes.filter(d => d.module === module_filter)
-						: this.doctypes;
-					this.setup_doctype_list();
+	load_doctypes(app_filter = null, module_filter = null) {
+		if (app_filter) {
+			// Load DocTypes by app
+			frappe.call({
+				method: 'data_tools.data_tools.page.partial_backup.partial_backup.get_doctypes_by_app',
+				args: {
+					app_name: app_filter
+				},
+				callback: (r) => {
+					if (r.message) {
+						this.doctypes = r.message;
+						this.filtered_doctypes = module_filter
+							? this.doctypes.filter(d => d.module === module_filter)
+							: this.doctypes;
+						this.setup_doctype_list();
+					}
 				}
-			}
+			});
+		} else {
+			// Load all DocTypes
+			frappe.call({
+				method: 'data_tools.data_tools.page.partial_backup.partial_backup.get_all_doctypes',
+				callback: (r) => {
+					if (r.message) {
+						this.doctypes = r.message;
+						this.filtered_doctypes = module_filter
+							? this.doctypes.filter(d => d.module === module_filter)
+							: this.doctypes;
+						this.setup_doctype_list();
+					}
+				}
+			});
+		}
+	}
+
+	setup_app_filter(apps) {
+		const container = this.page.main.find('#app-filter');
+
+		this.app_filter = frappe.ui.form.make_control({
+			parent: container,
+			df: {
+				fieldtype: 'Select',
+				fieldname: 'app',
+				options: ['All Apps', ...apps],
+				default: 'All Apps',
+				onchange: () => {
+					const selected_app = this.app_filter.get_value();
+					const app = selected_app === 'All Apps' ? null : selected_app;
+					const module = this.module_filter ? this.module_filter.get_value() : null;
+					const module_filter = module === 'All Modules' ? null : module;
+					this.load_doctypes(app, module_filter);
+				}
+			},
+			render_input: true
 		});
 	}
 
@@ -94,9 +164,32 @@ class PartialBackupPage {
 				options: ['All Modules', ...modules],
 				default: 'All Modules',
 				onchange: () => {
-					const selected = this.module_filter.get_value();
-					const filter = selected === 'All Modules' ? null : selected;
-					this.load_doctypes(filter);
+					const selected_module = this.module_filter.get_value();
+					const module = selected_module === 'All Modules' ? null : selected_module;
+					const app = this.app_filter ? this.app_filter.get_value() : null;
+					const app_filter = app === 'All Apps' ? null : app;
+					this.load_doctypes(app_filter, module);
+				}
+			},
+			render_input: true
+		});
+	}
+
+	setup_export_format() {
+		const container = this.page.main.find('#export-format');
+
+		this.export_format_control = frappe.ui.form.make_control({
+			parent: container,
+			df: {
+				fieldtype: 'Select',
+				fieldname: 'export_format',
+				options: [
+					{label: 'JSON (Metadata with records)', value: 'json'},
+					{label: 'SQL (Database dump)', value: 'sql'}
+				],
+				default: 'json',
+				onchange: () => {
+					this.export_format = this.export_format_control.get_value();
 				}
 			},
 			render_input: true
@@ -192,7 +285,8 @@ class PartialBackupPage {
 		frappe.call({
 			method: 'data_tools.data_tools.page.partial_backup.partial_backup.create_partial_backup',
 			args: {
-				doctypes: this.selected_doctypes
+				doctypes: this.selected_doctypes,
+				export_format: this.export_format
 			},
 			freeze: true,
 			freeze_message: __('Creating backup...'),
@@ -206,7 +300,12 @@ class PartialBackupPage {
 					for (let i = 0; i < binary_data.length; i++) {
 						array[i] = binary_data.charCodeAt(i);
 					}
-					const blob = new Blob([array], { type: 'application/zip' });
+
+					// Set correct MIME type based on export format
+					const mime_type = this.export_format === 'sql'
+						? 'application/sql'
+						: 'application/zip';
+					const blob = new Blob([array], { type: mime_type });
 					const url = window.URL.createObjectURL(blob);
 					const a = document.createElement('a');
 					a.href = url;
@@ -229,7 +328,7 @@ class PartialBackupPage {
 					});
 				}
 			},
-			error: (r) => {
+			error: () => {
 				status_elem.html('<span class="text-danger">Backup failed</span>');
 				frappe.msgprint(__('Error creating backup. Please check the error log.'));
 			}
